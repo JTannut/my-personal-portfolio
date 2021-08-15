1c639cd8a4778915571313cb7421cc65d16dc654
JTannut <tannut_tawonsan@hotmail.com>
2021-08-15 14:48:38 +0700

    add bootstrap

--- public/js/script.js
+++ public/js/script.js
@@ -33,5 +33,45 @@ $(document).ready(function(){
         }
     }
    });
+   $('.carouselskills').owlCarousel({
+    margin: 10,
+    loop: true,
+    autoplayTimeOut: 1300,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 1,
+            nav: false
+        },
+        1000:{
+            items: 1,
+            nav: false
+        }
+    }
+   });
+   $('.carouselcert').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 1,
+            nav: false
+        },
+        1000:{
+            items: 2,
+            nav: false
+        }
+    }
+   });
 });
 
